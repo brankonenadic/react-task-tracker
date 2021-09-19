74754d57563f40825d8e5f118b9fa84adfb13b8a
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-09-19 15:47:49 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect} from "react"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
+import About from "./components/AboutUs"
 
 function App() {
   const [showAddTask, setShowTask] = useState(false)
@@ -21,31 +24,97 @@ function App() {
     const data = await res.json()
     return data
   }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 //add task
-const addNewTask = (task) => {
-  const id = Math.floor(Math.random() * 100) + 1
+const addNewTask = async (task) => {
+
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+
+/*   const id = Math.floor(Math.random() * 100) + 1
   const newTask = { id, ...task}
-  setTasks([...tasks, newTask])
+  setTasks([...tasks, newTask]) */
  
 }
 
 // delete task 
-    const deletetask = (id) => {
+    const deletetask = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`,{
+        method: 'DELETE'
+      })
       setTasks(tasks.filter((task) => task.id !== id))
     }
   // toggle reimainder
-  const toggleReimainder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder } : task))
-  }
-  return (
-    <div className="container">
-        <Header onAdd={() => setShowTask(!showAddTask)} showAdd={showAddTask} />
-        {showAddTask && <AddTask onAdd={addNewTask} />}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deletetask} onToggle={toggleReimainder} />) : ('No tasks to show!')} 
-    </div>
-  );
-}
 
+  const toggleReimainder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
+
+ 
+return (
+  <Router>
+    <div className='container'>
+      <Header
+        onAdd={() => setShowTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      <Route
+        path='/'
+        exact
+        render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addNewTask} />}
+            {tasks.length > 0 ? (
+              <Tasks
+                tasks={tasks}
+                onDelete={deletetask}
+                onToggle={toggleReimainder}
+              />
+            ) : (
+              'No Tasks To Show'
+            )}
+          </>
+        )}
+      />
+      <Route path='/about' component={About} />
+      <Footer />
+    </div>
+  </Router>
+)
+}
 export default App;
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import  Buttons  from './Buttons'
 
 
 const Header = ({ title, onAdd, showAdd}) => {
+    const location = useLocation()
     return (
         <header className="header">
             <h1>{title}</h1>
-           <Buttons color={showAdd ? 'Red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+            {location.pathname === '/' && (
+       <Buttons color={showAdd ? 'Red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      )}
+           
         </header>
     )
 }
